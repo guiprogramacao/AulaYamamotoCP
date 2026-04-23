262121d77176cc53f5d5c899231a54c84e19c945
guiprogramacao <mirandaguilherm3@gmail.com>
2026-04-23 08:53:22 -0300

    Corrigindo o erro da falta da letra

--- src/codigo_com_erros.c
+++ src/codigo_com_erros.c
@@ -7,7 +7,7 @@ int main() {
     scanf("%f", &nota1);
 
     printf("Digite a segunda nota: ");
-    scanf("%f", nota2);
+    scanf("%f", &nota2); //Faltou o "E" comercial antes do nota 2
 
     media = nota1 + nota2 / 2;
 
