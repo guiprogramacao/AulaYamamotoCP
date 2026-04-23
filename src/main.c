2faf61de523c3e2603d0100fb86de5ab6b0c0330
28e8hrhr <tcarloseduardo802@gmail.com>
2026-04-23 09:47:53 -0300

    corrigino erros

--- src/main.c
+++ src/main.c
@@ -1,25 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 
+// Função para calcular média
 float calcularMedia(float n1, float n2, float n3) {
     return (n1 + n2 + n3) / 3;
 }
 
-void aprovar (float media){
-    if (media>=7 && media<=10){
-        printf("Aluno aprovado");
-    } else if (media <7 && media >= 5) {
-        printf("recuperacao");
+// Função para classificação
+void classificacao(float media) {
+    if (media < 0 || media > 10) {
+        printf("Media invalida\n");
+    } else if (media >= 7) {
+        printf("Aluno aprovado\n");
+    } else if (media >= 5) {
+        printf("Aluno em recuperacao\n");
     } else {
-        printf("reprovado");
-
+        printf("Aluno reprovado\n");
     }
 }
 
 // Função para derivada
 void calcularDerivada() {
-    int a; 
-    int b;
-    int c;
+    int a, b, c;
+
     printf("Digite o valor de a: ");
     scanf("%d", &a);
     printf("Digite o valor de b: ");
@@ -27,28 +29,27 @@ void calcularDerivada() {
     printf("Digite o valor de c: ");
     scanf("%d", &c);
 
-    printf("Função original: f(x) = %dx^2 + %dx + %d\n", a, b, c);
+    printf("Funcao original: f(x) = %dx^2 + %dx + %d\n", a, b, c);
 
-    if (b >= 0){
+    if (b >= 0) {
         printf("Derivada: f'(x) = %dx + %d\n", 2*a, b);
-    }
-     else{
+    } else {
         printf("Derivada: f'(x) = %dx - %d\n", 2*a, -b);
     }
-        
 }
-    
+
 int main() {
     int opcao;
 
     do {
         printf("\n=== MENU ===\n");
-        printf("1 - Calcular média\n");
-        printf("2 - Classificação acadêmica\n");
+        printf("1 - Calcular media\n");
+        printf("2 - Classificacao academica\n");
         printf("3 - Calcular derivada\n");
         printf("0 - Sair\n");
         printf("Escolha: ");
         scanf("%d", &opcao);
+
         switch(opcao) {
             case 1: {
                 float n1, n2, n3, media;
@@ -57,19 +58,20 @@ int main() {
                 scanf("%f %f %f", &n1, &n2, &n3);
 
                 media = calcularMedia(n1, n2, n3);
-                printf("Média = %.2f\n", media);
+                printf("Media = %.2f\n", media);
                 break;
             }
 
             case 2: {
                 float media;
 
-                printf("Digite a média: ");
+                printf("Digite a media: ");
                 scanf("%f", &media);
 
                 classificacao(media);
                 break;
             }
+
             case 3:
                 calcularDerivada();
                 break;
@@ -79,9 +81,10 @@ int main() {
                 break;
 
             default:
-                printf("Opção inválida!\n");
-        } 
+                printf("Opcao invalida!\n");
+        }
+
     } while(opcao != 0);
-    
+
     return 0;
 }
